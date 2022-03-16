51506c696e9ddb85499dd8baae6b5c06ca0e696e
Chandelier Ephrata <chandephrata@gmail.com>
2022-03-15 19:00:40 -0600

    commit tree 900f7fd7f3dc5110c3d086ab98f52c4f58213dd6

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter((match) => match.toLowerCase() === name.toLowerCase());
+ }
+
+ function fuzzyMatch(drivers,letters){
+    return drivers.filter(
+        (maybeMatch) =>
+          maybeMatch.toLowerCase().indexOf(letters.toLowerCase()) === 0
+      );
+    }
+
+function matchName(drivers, nameMatch){
+    return drivers.filter((record) => record.name === nameMatch);
+}
